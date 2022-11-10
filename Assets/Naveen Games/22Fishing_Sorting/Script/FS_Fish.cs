88bb04b16e82503286a7e22388029d8c19d1200c
karthickdl <karthick@dlearners.in>
2022-11-10 12:16:23 +0530

    Inital push

---
+++ Assets/Naveen Games/22Fishing_Sorting/Script/FS_Fish.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FS_Fish : MonoBehaviour
+{
+    bool B_CanMove;
+    public float speed=1f;
+    private void Start()
+    {
+        B_CanMove = true;
+    }
+    private void Update()
+    {
+        if(B_CanMove)
+        {
+            this.transform.Translate(Vector3.left *speed* Time.deltaTime);
+        }
+       
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name== "Rope_end")
+        {
+            if(Fish_sorting_main.Instance.B_CanCatched)
+            {
+               // Debug.Log("this == " + this.transform.GetChild(0).GetComponent<Text>().text);
+                Fish_sorting_main.Instance.STR_currentSelectedAnswer = this.transform.GetChild(0).GetComponent<Text>().text;
+                Fish_sorting_main.Instance.THI_Catched();
+                this.transform.GetChild(0).GetComponent<AudioSource>().Play();
+                
+                B_CanMove = false;
+                this.transform.SetParent(Fish_sorting_main.Instance.G_hook.transform.GetChild(0).transform, false);
+                this.transform.position = Fish_sorting_main.Instance.G_hook.transform.GetChild(0).transform.position;
+            }
+        }
+        if(collision.gameObject.name== "DestroyFish")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
